4655ff835bfbab2f6918d3de8fb3a5dafdbf2236
pjflux2001 <46859995+pjflux2001@users.noreply.github.com>
2019-07-07 23:40:20 +0530

    Add files via upload

---
+++ Web Assign/js/index.js
@@ -0,0 +1,14 @@
+document.getElementById("defaultOpen").click();
+
+function d() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  
+
+  
